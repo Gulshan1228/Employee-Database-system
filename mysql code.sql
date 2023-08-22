5188d5ca4ed56ab7a4f716fbbe6ce18b9953a276
Gulshan Singh <126390444+Gulshan1228@users.noreply.github.com>
2023-08-23 01:09:52 +0530

    Delete mysql code.sql

--- mysql code.sql
+++
@@ -1,8 +0,0 @@
-create database project;
-use project;
-
-create table info(Name varchar(25),Email varchar(20),Contact int(10),Gender varchar(8),Father_Name varchar(25),Address varchar(100),Post varchar(20));
-
-desc info;
-
-select * from info;
